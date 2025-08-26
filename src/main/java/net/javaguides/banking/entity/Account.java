66818588a55a26d2259ed497405707fd12f3dbf1
santosh <santoshpasi6566@gmail.com>
2025-08-27 00:54:31 +0530

    Entity Created

---
+++ src/main/java/net/javaguides/banking/entity/Account.java
@@ -0,0 +1,19 @@
+package net.javaguides.banking.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "accounts")
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "account_holder_name")
+    private String accountHolderName;
+    private double balance;
+}
