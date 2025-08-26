0bf0746db4f302ab87f5a0122477e03ab31a99fa
santosh <santoshpasi6566@gmail.com>
2025-08-27 01:41:51 +0530

    Added all dependency file for Controller

---
+++ src/main/java/net/javaguides/banking/controller/AccountController.java
@@ -0,0 +1,17 @@
+package net.javaguides.banking.controller;
+
+import net.javaguides.banking.service.AccountService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+    private AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+    // Add Account REST API
+
+}
